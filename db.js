c1213bc8482568d2531b84c3b41e7ec70cc096e2
antomasi9 <masilizaantonella@gmail.com>
2022-04-14 17:49:02 -0300

    Primer entrega

---
+++ db.js
@@ -0,0 +1,63 @@
+const pictures = [
+    {
+      id: 1,
+      url: "./images/AnnaTis/1.jpg",
+      author: "Anna Tis",
+    },
+    {
+      id: 2,
+      url: "./images/AnnaTis/2.jpg",
+      author: "Anna Tis",
+    },
+    {
+      id: 3,
+      url: "./images/AnnaTis/3.jpg",
+      author: "Anna Tis",
+    },
+    {
+      id: 4,
+      url: "./images/MaksimRomashkin/1.jpg",
+      author: "Maksim Romashkin",
+    },
+    {
+      id: 5,
+      url: "./images/MaksimRomashkin/2.jpg",
+      author: "Maksim Romashkin",
+    },
+    {
+      id: 6,
+      url: "./images/MaksimRomashkin/3.jpeg",
+      author: "Maksim Romashkin",
+    },
+    {
+      id: 7,
+      url: "./images/Studio31/1.jpg",
+      author: "Studio 31",
+    },
+    {
+      id: 8,
+      url: "./images/Studio31/2.jpg",
+      author: "Studio 31",
+    },
+    {
+      id: 9,
+      url: "./images/Studio31/3.jpg",
+      author: "Studio 31",
+    },
+    {
+      id: 10,
+      url: "./images/ValeriyaKobzar/1.jpg",
+      author: "Valeriya Kobzar",
+    },
+    {
+      id: 11,
+      url: "./images/ValeriyaKobzar/2.jpg",
+      author: "Valeriya Kobzar",
+    },
+    {
+      id: 12,
+      url: "./images/ValeriyaKobzar/3.jpg",
+      author: "Valeriya Kobzar",
+    },
+  ];
+  
---
+++ index.js
@@ -0,0 +1,43 @@
+const nombre = prompt("¿Cómo te llamas?");
+const edad = prompt("¿Cuántos años tienes?");
+
+const calcularGanancia = (monto) => {
+  const breakPoints = [100, 500, 1500, 5000, 10000, 50000];
+  const interes = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3];
+  const ganancia = [];
+  for (let i = 0; i < interes.length; i++) {
+    ganancia.push(monto * interes[i]);
+  }
+  switch (true) {
+    case monto <= breakPoints[0]:
+      console.log(`Con $${monto} obtenés $${ganancia[0]} de ganancia. case1`);
+      break;
+    case monto > breakPoints[0] && monto <= breakPoints[1]:
+      console.log(`Con $${monto} obtenés $${ganancia[1]} de ganancia. case2`);
+      break;
+    case monto > breakPoints[1] && monto <= breakPoints[2]:
+      console.log(`Con $${monto} obtenés $${ganancia[2]} de ganancia. case3`);
+      break;
+    case monto > breakPoints[2] && monto <= breakPoints[3]:
+      console.log(`Con $${monto} obtenés $${ganancia[3]} de ganancia. case4`);
+      break;
+    case monto > breakPoints[3] && monto <= breakPoints[4]:
+      console.log(`Con $${monto} obtenés $${ganancia[4]} de ganancia. case5`);
+      break;
+    default:
+      console.log(`Con $${monto} obtenés $${ganancia[5]} de ganancia. case6`);
+      break;
+  }
+};
+
+if (edad < 18) {
+  console.log(
+    `Hola ${nombre}, por tu edad no es conveniente que veas el contenido de nuestra página. Lo lamentamos.`
+  );
+} else {
+  alert(`Hola ${nombre}, tienes ${edad} años. ¡Disfruta nuestro contenido!`);
+  let monto = prompt("¿Cuánto dinero quieres invertir?");
+  const montoNumber = parseInt(monto);
+
+  calcularGanancia(montoNumber);
+}
